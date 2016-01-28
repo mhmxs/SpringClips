81c4d417ff7e9aa59e9ad7c4367c134fb2ff077f
mhmxs <kovacsricsi@gmail.com>
2016-01-28 10:09:29 +0100

    Transaction manager configured

--- src/main/java/com/github/springclips/configurations/PersistenceConfigurarion.java
+++ src/main/java/com/github/springclips/configurations/PersistenceConfigurarion.java
@@ -3,10 +3,24 @@ package com.github.springclips.configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Configuration
 public class PersistenceConfigurarion {
@@ -20,4 +34,54 @@ public class PersistenceConfigurarion {
 
         return factory.createEntityManager();
     }
+
+    @Bean
+    public DataSource dataSource() {
+        final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+
+        return builder.setType(EmbeddedDatabaseType.DERBY).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final EclipseLinkJpaVendorAdapter vendorAdapter = new EclipseLinkJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.DERBY);
+        vendorAdapter.setGenerateDdl(true);
+
+        final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan(getClass().getPackage().getName());
+        factory.setDataSource(dataSource());
+
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        final JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return txManager;
+    }
+
+    @Bean
+    @Lazy(false)
+    public ResourceDatabasePopulator populateDatabase() throws SQLException {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("data.sql"));
+
+        Connection connection = null;
+
+        try {
+            connection = DataSourceUtils.getConnection(dataSource());
+            populator.populate(connection);
+        } finally {
+            if (connection != null) {
+                DataSourceUtils.releaseConnection(connection, dataSource());
+            }
+        }
+
+        return populator;
+    }
+
 }
